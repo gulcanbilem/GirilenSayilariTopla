7651d04e6b3ea92620b896ddd79d4ae9d0efa0ae
gulcanbilem <113993110+gulcanbilem@users.noreply.github.com>
2023-01-01 10:40:50 +0300

    Add files via upload

---
+++ GirilenSayilariTopla/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirilenSayilariTopla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1,sayi2;
+            Console.WriteLine("1. Sayıyı Giriniz:");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. Sayıyı Giriniz:");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            int sonuc = sayi1 + sayi2;
+
+            if (sayi2 == 83)
+                Console.WriteLine("Girilen sayıların toplamı" + sonuc);
+                    Console.WriteLine("Döngü Bitti.");
+            Console.ReadLine();
+
+                
+
+
+
+
+
+        }
+    }
+}
